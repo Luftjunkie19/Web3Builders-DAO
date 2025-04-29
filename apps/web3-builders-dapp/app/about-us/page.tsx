25d626eb34afe83709c253c8e0a5e967172f7a0c
Luftjunkie19 <kshjssjjs@gmail.com>
2025-04-29 13:49:44 +0200

    Adjust the About Page to the needs

--- apps/web3-builders-dapp/app/about-us/page.tsx
+++ apps/web3-builders-dapp/app/about-us/page.tsx
@@ -1,12 +1,17 @@
 import { AuroraText } from '@/components/magicui/aurora-text'
 import { BlurFade } from '@/components/magicui/blur-fade'
 import { BorderBeam } from '@/components/magicui/border-beam'
+import { IconCloud } from '@/components/magicui/icon-cloud'
 import { SparklesText } from '@/components/magicui/sparkles-text'
-import { CanvasRevealEffect } from '@/components/ui/canvas-reveal-effect'
-import { Card } from '@/components/ui/card'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 import { FocusCards } from '@/components/ui/focus-cards'
-import { HeroHighlight } from '@/components/ui/hero-highlight'
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 import React from 'react'
@@ -14,6 +19,50 @@ import React from 'react'
 type Props = {}
 
 function Page({}: Props) {
+
+  const slugs = [
+  "solidity",
+  "nodejs",
+  "typescript",
+  "python",
+  "javascript",
+  "rust",
+  "go",
+  "java",
+  "csharp",
+  "php",
+  "swift",
+  "kotlin",
+  "dart",
+  "ruby",
+  "html",
+  "css",
+  "react",
+  "vue",
+  "angular",
+  "svelte",
+  "nextjs",
+  "nuxtjs",
+  "express",
+  "fastify",
+  "flask",
+  "django",
+  "laravel",
+  "spring",
+  "rails",
+  "aspnet",
+  "symfony",
+  "dotnet",
+  "git",
+  "github",
+  "move"
+  ];
+ 
+  const images = slugs.map(
+    (slug) => `https://cdn.simpleicons.org/${slug}/${slug}/:light`,
+  );
+
+
   return (
     <div className="w-full h-full">
       
@@ -22,7 +71,7 @@ function Page({}: Props) {
 <div className="flex flex-col items-center gap-3">
 <BlurFade>
 <div className='text-white text-5xl font-bold flex flex-wrap gap-2'>Welcome to <SparklesText sparklesCount={10} colors={{first:'#05F29B', second: '#03A678'}} className='text-5xl font-bold'>
-<AuroraText colors={['#2289ff', '#6e2094']}>Web3</AuroraText>Builders DAO
+<AuroraText colors={['#2289ff', '#6e2094']}>Web3</AuroraText>Builders 
 </SparklesText></div>
 </BlurFade>
 
@@ -32,26 +81,161 @@ function Page({}: Props) {
         </div>
 
 
-<div className="flex w-full max-w-6xl my-24 justify-between items-center  gap-24 ">
+<div className="flex w-full max-w-6xl my-12 mx-auto justify-between items-center gap-24
+flex-col md:flex-row">
 <BlurFade delay={0.5}>
   <BorderBeam colorFrom='#05F29B' colorTo='#03A678'/>
 <Image alt='' className='max-w-sm w-full rounded-lg' src={'/Web3Builders.png'} width={300} height={300}/>
 </BlurFade>
 <BlurFade delay={0.85}>
   <div className="flex  max-w-xl w-full flex-col gap-5 text-white">
-<p className='text-2xl font-bold'>What is <span className='p-2 rounded-lg text-zinc-800 text-animated-first-section'>Web3Builders DAO</span> ?</p>
+<p className='text-2xl font-bold'>What is <span className='p-2 rounded-lg text-zinc-800 text-animated-first-section'>Web3Builders</span> ?</p>
 <p className=' leading-8'>
-  Web3Builders DAO is a community, focused on builders, developers, and enthusiasts of Web3, technology, programming and most importantly, <span className='p-2 text-(--hacker-green-4) bg-zinc-800 rounded-lg'>Blockchain</span>. 
+  Web3Builders is a community, focused on builders, developers, and enthusiasts of Web3, technology, programming and most importantly, <span className='p-2 text-(--hacker-green-4) bg-zinc-800 rounded-lg'>Blockchain</span>. 
 </p>
 <p className='leading-8'>Our Community's purpose is to create a safe, friendly, relatively small-sized community and welcoming environment for all our members to grow and learn. What should excel us from the rest ? That you can have influence on the server's decision making process.</p>
   </div>
 </BlurFade>
 </div>
 
-<div className="flex text-white flex-col gap-2">
-  <p className='text-3xl font-bold'>The Founders</p>
+<div className="flex w-full max-w-6xl my-12 justify-between items-center gap-16
+flex-col md:flex-row
+
+">
+<BlurFade>
+<div className="flex max-w-md w-full flex-col gap-5">
+<IconCloud
+images={images} />
+</div>
+</BlurFade>
+
+
+<div className="max-w-2xl w-full flex flex-col gap-5 text-white">
+  <p
+  className='text-3xl font-bold'
+  > Who is the community for ?</p>
+  <p className='leading-7'>
+    Unfortunately we are not a community for everyone. We are a community for developers, builders and enthusiasts of Web3, technology, programming and most importantly, Blockchain. We are not a community for people who just want to chill and have fun. We are a community for people who want to learn, grow and build together and have fun simultaneously 😎 (We're not numb though). We are a community for people who want to make an impact in the world of Web3. If you are one of those people, then you are in the right place.
+  </p>
+</div>
+</div>
+
+
+
+<div className="flex text-white flex-col gap-2 w-full items-center  max-w-6xl my-12
+p-3
+">
+  <p className='text-4xl font-bold
+  '>The Founders</p>
   <p>Let's introduce the Founders of Web3Builders, the greatest community of developers, builders and enthusiasts of Web3. Real heros and not just sweet-unicorny talkers 🥰</p>
-  <FocusCards  cards={[{'title':'Akash Dutta', src:'/founders/Akash.jpg', description:'Founder of Web3Builders, Idea Maker, Web3 Enthusiast'}, {'src':'/founders/luftjunkie-1.jpg', title:'Luftjunkie', description:"Co-Founder of Web3Builders, DAO Creator, Fullstack Blockchain Developer"},{'title':'Arcane', src:'/founders/Arcane-Jose.jpg', description:'CTO of Web3Builders, Senior Blockchain Developer, Web3 Enthusiast'}]}/>
+
+  <FocusCards cards={[{'title':'Akash Dutta', src:'/founders/Akash.jpg', description:'Founder of Web3Builders, Idea Maker, Web3 Enthusiast'}, {'src':'/founders/luftjunkie-1.jpg', title:'Luftjunkie', description:"Co-Founder of Web3Builders, DAO Creator, Fullstack Blockchain Developer"},{'title':'Arcane', src:'/founders/Arcane-Jose.jpg', description:'CTO of Web3Builders, Senior Blockchain Developer, Web3 Enthusiast'}]}/>
+
+</div>
+
+
+<div className="w-full max-w-5xl mx-auto flex flex-col gap-5 items-center">
+<p
+className='text-4xl font-bold text-white'>Frequently Asked Questions</p>
+<p className='text-white'>We are a new community, so we don't have many questions yet. But we will add more questions as we grow.
+  But here are some that might pop into your head 😎
+</p>
+
+<Accordion type="multiple" className=' max-w-3xl w-full bg-zinc-800 rounded-lg text-white
+'>
+  <AccordionItem value="item-1" className='p-2'>
+    <AccordionTrigger
+    className='text-white
+    text-xl 
+    hover:no-underline
+    bg-zinc-800 hover:bg-zinc-700 rounded-lg p-2'>
+      <p className='flex items-center gap-2 text-wrap flex-wrap'>
+      Who can join
+ <span className='text-(--hacker-green-4) '>
+  Web3Builders
+ </span>
+    ?
+      </p>
+    </AccordionTrigger>
+    <AccordionContent
+    className='text-white bg-zinc-800 hover:bg-zinc-700 rounded-md p-2'>
+  Well firstly, we have to decide whether we take new people or not. For now (29.04.2024) we are not taking new people.
+  As the server is not built yet. But if you are a developer, builder or enthusiast of Web3, technology, programming and most importantly, Blockchain, you can message us and we will consider with the community, if we make the exception or not 😉
+    </AccordionContent>
+  </AccordionItem>
+
+
+  <AccordionItem value="item-2" className='p-2'>
+    <AccordionTrigger
+    className='text-white
+    text-xl 
+    hover:no-underline
+    bg-zinc-800 hover:bg-zinc-700 rounded-lg p-2'>
+      <p className='flex items-center gap-2 text-wrap flex-wrap'>
+  How large is
+ <span className='text-(--hacker-green-4) '>
+  Web3Builders
+ </span>
+ going to be 
+    ?
+      </p>
+    </AccordionTrigger>
+    <AccordionContent
+    className='text-white bg-zinc-800 hover:bg-zinc-700 rounded-md p-2'>
+  We're not going to be a large community like (5k) members but in fact I want to keep it small and friendly.
+  As the first aim we target a number of 10 members (29.04.2024).
+  We want to create a community where you can actually know the people in the community and not just be a number.
+  We want to create a community where you can actually make an impact on the server and not be a slave of admins and mods. We 
+  want to create a community where you can actually have fun with learning and growing and not get overwhelmed, when the answers are not clear.
+    </AccordionContent>
+  </AccordionItem>
+
+
+  
+  <AccordionItem value="item-3" className='p-2'>
+    <AccordionTrigger
+    className='text-white
+    text-xl 
+    hover:no-underline
+    bg-zinc-800 hover:bg-zinc-700 rounded-lg p-2'>
+      <p className='flex items-center gap-2 text-wrap flex-wrap'>
+What are the requirements to join
+ <span className='text-(--hacker-green-4) '>
+  Web3Builders
+ </span>
+    ?
+      </p>
+    </AccordionTrigger>
+    <AccordionContent
+    className='text-white bg-zinc-800 hover:bg-zinc-700 rounded-md p-2'>
+  The requirements are pretty straight-forward. You have to be:
+  <ul className='list-disc list-inside'>
+    <li>
+      A person who is interested in Web3, technology, programming and most importantly, Blockchain.
+    </li>
+
+    <li>
+     A developer, a person with programming-background or just starting with programming with aim to contribute to Web3 or already contributing to Web3.
+    </li>
+
+<li>
+  A guy is intending to take part in the community's events like building projects, a bit of competitions, internal-hackathons, etc.
+</li>
+
+    <li>
+   A person who is fully devoted to Web3Builders and doesn't treat this community as a joke or a game.
+    </li>
+
+    <li>A person, who
+      is willing to grow, learn and build together with the community.
+    </li>
+
+
+  </ul>
+    </AccordionContent>
+  </AccordionItem>
+
+</Accordion>
 </div>
 
 
--- apps/web3-builders-dapp/components/ui/focus-cards.tsx
+++ apps/web3-builders-dapp/components/ui/focus-cards.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { cn } from "@/lib/utils";
 import { BlurFade } from "../magicui/blur-fade";
+import { useIsMobile } from '../../hooks/use-mobile';
 
 export const Card = React.memo(
   ({
@@ -11,11 +12,13 @@ export const Card = React.memo(
     hovered,
     description,
     setHovered,
+    isMobile
   }: {
     card: any;
     index: number;
     hovered: number | null;
     description?: string;
+    isMobile: boolean;
     setHovered: React.Dispatch<React.SetStateAction<number | null>>;
   }) => (
     <BlurFade delay={0.5 * index}>
@@ -23,14 +26,17 @@ export const Card = React.memo(
       onMouseEnter={() => setHovered(index)}
       onMouseLeave={() => setHovered(null)}
       className={cn(
-        "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
-        hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
+        "rounded-lg  self-center relative group bg-gray-100 dark:bg-neutral-900 hover:rotate-[-6deg] hover:scale-105 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
+        hovered !== null && hovered !== index && "blur-sm scale-[0.98]",
+        isMobile && "h-96 max-w-md w-full self-center"
       )}
     >
       <img
         src={card.src}
         alt={card.title}
-        className="object-cover h-full absolute inset-0"
+        className="object-cover group-hover:scale-[1.5] group-hover:rotate-12 transition-all duration-500 h-full absolute inset-0
+        w-full
+        "
       />
       <div
         className={cn(
@@ -43,7 +49,7 @@ export const Card = React.memo(
           {card.title}
         </div>
         {description && (
-          <div className="text-lg font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">{description}</div>
+          <div className="text-sm font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">{description}</div>
         )}
       </div>
       </div>
@@ -62,15 +68,18 @@ type Card = {
 
 export function FocusCards({ cards }: { cards: Card[] }) {
   const [hovered, setHovered] = useState<number | null>(null);
+  const isMobile = useIsMobile();
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto md:px-8 w-full">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto md:px-8 py-6 w-full 
+    ">
       {cards.map((card, index) => (
         <Card
           key={card.title}
           card={card}
           index={index}
           hovered={hovered}
+          isMobile={isMobile}
           description={card.description}
           setHovered={setHovered}
         />
