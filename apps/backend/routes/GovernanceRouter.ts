e70442c70f48042c2665c5d63255e49b77516a26
Luftjunkie19 <kshjssjjs@gmail.com>
2025-05-12 23:10:36 +0200

    feat: add governance and token distribution routes and controllers

---
+++ apps/backend/routes/GovernanceRouter.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { activateProposal, cancelProposal, executeProposal, getProposalQuorum, getProposalState, getProposalVotes, queueProposal } from "../controllers/GovernanceController";
+
+const governanceRouter = Router();
+
+
+governanceRouter.post('/activate_proposal/:proposalId',activateProposal);
+
+governanceRouter.post('/queue_proposal/:proposalId', queueProposal);
+
+governanceRouter.post('/cancel_proposal/:proposalId', cancelProposal);
+
+governanceRouter.post('/execute_proposal/:proposalId', executeProposal);
+
+governanceRouter.get('/get_proposal_votes/:proposalId', getProposalVotes);
+
+governanceRouter.get('/get_proposal_state/:proposalId', getProposalState);
+
+governanceRouter.get('/get_proposal_quorum_needed/:proposalId', getProposalQuorum);
+
+export default governanceRouter;
