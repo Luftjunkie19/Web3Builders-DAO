0fe16dd7c8c2bd4bbac8fbfabed99d11b2781288
Luftjunkie19 <kshjssjjs@gmail.com>
2025-06-06 13:01:30 +0200

    feat: enhance proposal list component with loading skeletons for improved user experience

--- apps/web3-builders-dapp/components/home/proposal-list/ProposalList.tsx
+++ apps/web3-builders-dapp/components/home/proposal-list/ProposalList.tsx
@@ -6,6 +6,7 @@ import ProposalElement from '@/components/proposal-item/ProposalElement'
 import useRealtimeDocuments from '@/hooks/useRealtimeDocuments'
 import useGetLoggedInUser from '@/hooks/useGetLoggedInUser';
 import { useAccount } from 'wagmi';
+import { Skeleton } from '@/components/ui/skeleton';
 
 type Props = {
     proposals:any[]
@@ -19,11 +20,55 @@ function ProposalList({proposals}: Props) {
 
   return (
     <>
-     <p onClick={()=>{console.log(serverData)}} className='text-white text-2xl font-semibold '>List with current proposals</p>
-       <DropdownBar/>
-    <div className="flex flex-col overflow-y-auto items-center gap-6  w-full">
+     
+          {serverData && currentUser && !isLoading && <>
+          <p onClick={()=>{console.log(serverData)}} className='text-white text-2xl font-semibold '>List with current proposals</p>
+          <DropdownBar/>
+          </>
+          }
+    <div className="flex flex-col overflow-y-auto items-center gap-6 w-full">
 
-    {serverData && !currentUser && !isLoading && serverData.map((proposal,index)=>(<ProposalElement proposalObj={proposal} key={proposal.proposal_id} proposalId={proposal.proposal_id} />))}
+{isLoading && (serverData || !serverData) && <>
+<Skeleton className='w-full max-w-2xl  bg-zinc-800 rounded-lg border border-green-400'>
+<div className='w-full flex items-center gap-4 p-4'>
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+<Skeleton className='w-32 h-4 bg-zinc-400 rounded' />
+</div>
+<Skeleton className='h-64 p-2 bg-zinc-700'/>
+<div className="w-full flex items-center gap-4 p-4">
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+</div>
+</Skeleton>
+
+<Skeleton className='w-full max-w-2xl  bg-zinc-800 rounded-lg border border-green-400'>
+<div className='w-full flex items-center gap-4 p-4'>
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+<Skeleton className='w-32 h-4 bg-zinc-400 rounded' />
+</div>
+<Skeleton className='h-64 p-2 bg-zinc-700'/>
+<div className="w-full flex items-center gap-4 p-4">
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+</div>
+</Skeleton>
+
+<Skeleton className='w-full max-w-2xl  bg-zinc-800 rounded-lg border border-green-400'>
+<div className='w-full flex items-center gap-4 p-4'>
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+<Skeleton className='w-32 h-4 bg-zinc-400 rounded' />
+</div>
+<Skeleton className='h-64 p-2 bg-zinc-700'/>
+<div className="w-full flex items-center gap-4 p-4">
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+<Skeleton className='w-8 h-8 bg-zinc-600 rounded-full' />
+</div>
+</Skeleton>
+</>}
+    {serverData && currentUser && !isLoading && serverData.map((proposal,index)=>(<ProposalElement proposalObj={proposal} key={proposal.proposal_id} proposalId={proposal.proposal_id} />))}
     
     </div>
 
