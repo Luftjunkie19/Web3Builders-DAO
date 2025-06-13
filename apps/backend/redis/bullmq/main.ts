fbd677a9302f06212749d954b8f15efa208056f3
Luftjunkie19 <kshjssjjs@gmail.com>
2025-06-13 23:36:35 +0200

    feat: update smartContractsInteracionQueue intervals for activate, finish, queue, and execute proposals

--- apps/backend/redis/bullmq/main.ts
+++ apps/backend/redis/bullmq/main.ts
@@ -19,10 +19,10 @@ await smartContractsInteracionQueue.remove("execute-proposals");
 await smartContractsInteracionQueue.setGlobalConcurrency(5);
 
 await smartContractsInteracionQueue.add("monthly-distribution", {},{ repeat: {'every': 1000 * 60 * 60 * 24 * 30 }, removeOnComplete: true,  });
-await smartContractsInteracionQueue.add("activate-proposals", {},{ repeat: {'every': 1000 * 60 * 15 }, removeOnComplete: true, });
-await smartContractsInteracionQueue.add("finish-proposals", {},{ repeat: {'every': 1000 * 60 * 15 }, 'delay':1000, removeOnComplete: true,  });
-await smartContractsInteracionQueue.add("queue-proposals", {},{ repeat: {'every': 1000 * 60 * 15  }, 'delay': 2000 , removeOnComplete: true, });
-await smartContractsInteracionQueue.add("execute-proposals", {},{ repeat: {'every': 1000 * 60 * 15  }, 'delay': 3000,removeOnComplete: true,  });
+await smartContractsInteracionQueue.add("activate-proposals", {},{ repeat: {'every': 1000 * 60 * 2 }, removeOnComplete: true, });
+await smartContractsInteracionQueue.add("finish-proposals", {},{ repeat: {'every': 1000 * 60 * 2 }, 'delay':1000, removeOnComplete: true,  });
+await smartContractsInteracionQueue.add("queue-proposals", {},{ repeat: {'every': 1000 * 60 * 2  }, 'delay': 2000 , removeOnComplete: true, });
+await smartContractsInteracionQueue.add("execute-proposals", {},{ repeat: {'every': 1000 * 60 * 2  }, 'delay': 3000,removeOnComplete: true,  });
 
 
 
