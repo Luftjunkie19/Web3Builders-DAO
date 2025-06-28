21cd7394e7a81a714ae834387128f0007d59854b
Luftjunkie19 <kshjssjjs@gmail.com>
2025-06-28 08:53:51 +0200

    fix: correct backend endpoint in DeleteAccount component

--- apps/web3-builders-dapp/components/settings/DeleteAccount.tsx
+++ apps/web3-builders-dapp/components/settings/DeleteAccount.tsx
@@ -26,7 +26,7 @@ try{
         toast.success('You are not logged in or no such user in DB !');
         return;
     }
-      const fetchDelete=await fetch(`${process.env.BACKEND_ENDPOINT_1}/gov_token/influence/remove/${currentUser.discord_member_id}`, {
+      const fetchDelete=await fetch(`${process.env.BACKEND_ENDPOINT}/gov_token/influence/remove/${currentUser.discord_member_id}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
